cfd4e173309552b3a648975fd1b60c056be14996
Johnthomas Lowing <johnthomaslowing@gmail.com>
2020-05-10 17:41:31 -0700

    Cleaning.

--- menu_stuff.js
+++
@@ -1,8 +0,0 @@
-function changeText(id){
-	var x = document.getElementById("textTest");
-	if(id == "saltNicButton"){
-		x.innerHTML = "You've selected the salt nic menu.";
-	} else if (id == "freebaseButton"){
-		x.innerHTML = "You've selected the freebase menu."
-	}
-}
