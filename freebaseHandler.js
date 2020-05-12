d52af27e80952857a41403667739571ca8d54536
Johnthomas Lowing <johnthomaslowing@gmail.com>
2020-05-12 16:08:52 -0700

    converted libraries to js array

---
+++ freebaseHandler.js
@@ -0,0 +1,37 @@
+let freebaseLibrary = [
+  'air_factory.jpg',
+  'alternativ.jpg',
+  'aqua.jpg',
+  'barista_brew.jpg',
+  'fresh_pressed.jpg',
+  'glas_basix.jpg',
+  'jam_monster.jpg',
+  'juice_head.jpg',
+  'kilo_sour_series.jpg',
+  'loyalty.jpg',
+  'marina.jpg',
+  'met4.jpg',
+  'naked.jpg',
+  'naked_cream.jpg',
+  'naked_menthol.jpg',
+  'one_up_vapor.jpg',
+  'pacific_northwest_one.jpg',
+  'pacific_northwest_two.jpg',
+  'reds_apple.jpg',
+  'ripe_collection.jpg',
+  'river_reserve_one.jpg',
+  'river_reserve_two.jpg',
+  'ruthless_one.jpg',
+  'ruthless_two.jpg',
+  'shijin.jpg',
+  'silverback.jpg',
+  'transistor.jpg'
+];
+
+freebaseLibrary.forEach(imageSrc => {
+  $('freebaseContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/freebase_images/${imageSrc} class = padding></div>`;
+});
+
+function $(query){
+	return document.getElementById(query);
+}
---
+++ saltsHandler.js
@@ -0,0 +1,19 @@
+let nicSaltLibrary = [
+  'air_factory_salts.jpg',
+  'glas_basix_salts.jpg',
+  'juice_head_salts.jpg',
+  'met4_salts.jpg',
+  'naked_salts.jpg',
+  'reds_salts.jpg',
+  'river_reserve_salts.jpg',
+  'shijin_salts.jpg',
+  'svrf_salts.jpg',
+];
+
+nicSaltLibrary.forEach(imageSrc => {
+  $('nicSaltContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/salts_images/${imageSrc} class = padding></div>`;
+});
+
+function $(query){
+	return document.getElementById(query);
+}
