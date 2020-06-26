fbcf1d0491a978ca3d2879fbd47d9cd93334aa44
Johnthomas Lowing <johnthomaslowing@gmail.com>
2020-06-26 02:41:55 -0700

    search for freebase

--- freebaseHandler.js
+++ freebaseHandler.js
@@ -1,5 +1,7 @@
+function $(que){
+  return document.getElementById(que);
+}
 let freebaseLibrary = [
-  {imageName: 'alternativ.jpg', profile: ["menthol"]},
   {imageName: 'aqua.jpg', profile: ["fruit"]},
   {imageName: 'barista_brew.jpg', profile: ["dessert"]},
   {imageName: 'glas_basix.jpg', profile: ["fruit", "menthol"]},
@@ -10,29 +12,42 @@ let freebaseLibrary = [
   {imageName: 'marina.jpg', profile: ["fruity"]},
   {imageName: 'met4.jpg', profile: ["fruity"]},
   {imageName: 'naked.jpg', profile: ["fruity"]},
-  {imageName: 'naked_cream.jpg', profile: "fruity"},
-  {imageName: 'naked_menthol.jpg', profile: "fruity"},
-  {imageName: 'one_up_vapor.jpg', profile: "fruity"},
-  {imageName: 'pacific_northwest_one.jpg', profile: "fruity"},
-  {imageName: 'pacific_northwest_two.jpg', profile: "fruity"},
-  {imageName: 'reds_apple.jpg', profile: "fruity"},
-  {imageName: 'ripe_collection.jpg', profile: "fruity"},
-  {imageName: 'river_reserve_two.jpg', profile: "fruity"},
-  {imageName: 'river_reserve_one.jpg', profile: "fruity"},
-  {imageName: 'ruthless_one.jpg', profile: "fruity"},
-  {imageName: 'ruthless_two.jpg', profile: "fruity"},
-  {imageName: 'shijin.jpg', profile: "fruity"},
-  {imageName: 'silverback.jpg', profile: "fruity"},
-  {imageName: 'transistor.jpg', profile: "fruity"},
-  {imageName: 'air_factory.jpg', profile: "fruity"},
-  {imageName: 'vapetasia.jpg', profile: "fruity"},
+  {imageName: 'naked_cream.jpg', profile: ["fruity"]},
+  {imageName: 'naked_menthol.jpg', profile: ["fruity"]},
+  {imageName: 'one_up_vapor.jpg', profile: ["fruity"]},
+  {imageName: 'pacific_northwest_one.jpg', profile: ["fruity"]},
+  {imageName: 'pacific_northwest_two.jpg', profile: ["fruity"]},
+  {imageName: 'reds_apple.jpg', profile: ["fruity"]},
+  {imageName: 'ripe_collection.jpg', profile: ["fruity"]},
+  {imageName: 'river_reserve_two.jpg', profile: ["fruity"]},
+  {imageName: 'river_reserve_one.jpg', profile: ["fruity"]},
+  {imageName: 'ruthless_one.jpg', profile: ["fruity"]},
+  {imageName: 'ruthless_two.jpg', profile: ["fruity"]},
+  {imageName: 'shijin.jpg', profile: ["fruity"]},
+  {imageName: 'silverback.jpg', profile: ["fruity"]},
+  {imageName: 'transistor.jpg', profile: ["fruity"]},
+  {imageName: 'air_factory.jpg', profile: ["fruity"]},
+  {imageName: 'vapetasia.jpg', profile: ["fruity"]},
 ];
-freebaseLibrary.sort();
+drawLibrary(freebaseLibrary);
 
-freebaseLibrary.forEach(imageSrc => {
-  $('freebaseContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/freebase_images/${imageSrc.imageName} class = padding></div>`;
+const searchBar = $("freebaseSearch");
+searchBar.addEventListener("keyup", e => {
+  const searchString = e.target.value.toLowerCase();
+  $('freebaseContainer').innerHTML = "";
+
+  const found = freebaseLibrary.filter(flavor => {
+    return (
+      flavor.imageName.includes(searchString) ||
+      flavor.profile.includes(searchString)
+    );
+  });
+  drawLibrary(found);
 });
 
-function $(query){
-	return document.getElementById(query);
+function drawLibrary(q){
+  q.forEach(imageSrc => {
+      $('freebaseContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/freebase_images/${imageSrc.imageName} class = padding></div>`;
+  });
 }
+
