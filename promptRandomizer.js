b24778cb47688a0e87d9019e3ee16fefad4861e7
Johnthomas Lowing <johnthomaslowing@gmail.com>
2020-06-27 01:19:28 -0700

    search bar for salts

--- promptRandomizer.js
+++ promptRandomizer.js
@@ -3,12 +3,19 @@ function $(que){
 }
 //holds random placeholders
 let placeholderLibrary = ["What does               taste like?",
-	"Something that tastes...",
-	"What flavors does                   make?"]
+	"Something that tastes like...",
+	"What flavors does                   make?",
+	"Try searching by strength like '12mg'",
+	"Try searching for 'menthol' flavors",
+	"Search for individual flavors like 'apple'"]
 var placeholderLength = placeholderLibrary.length;
 //generates random number for to determine placeholder
 function randomNum(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 //executes change
-document.getElementById("freebaseSearch").placeholder = placeholderLibrary[randomNum(placeholderLength)];
+if(document.URL.includes("freebase.html")){
+	$("freebaseSearch").placeholder = placeholderLibrary[randomNum(placeholderLength)];
+} else if (document.URL.includes("nic_salts.html")){
+	$("saltSearch").placeholder = placeholderLibrary[randomNum(placeholderLength)];
+}
--- saltsHandler.js
+++ saltsHandler.js
@@ -1,18 +1,51 @@
-let nicSaltLibrary = [
-  'salt_factory.jpg',
-  'glas_basix_salts.jpg',
-  'juice_head_salts.jpg',
-  'met4_salts.jpg',
-  'naked_salts.jpg',
-  'reds_salts.jpg',
-  'river_reserve_salts.jpg',
-  'shijin_salts.jpg',
-  'svrf_salts.jpg',
+let saltLibrary = [
+  {menuCard: 'glas_basix_salts.jpg', strength:["30mg", "50mg"], profile: ["fruit", "menthol", "dessert"], flavors: ["juicy apple", "mango tango", "cool melon", "fizzy lemonade", "banana cream pie", "blue magic", "crunch berry"]},
+  {menuCard: 'juice_head_salts.jpg', strength:["25mg", "50mg"], profile: ["fruit"], flavors: ["strawberry kiwi", "peach pear", "pineapple grapefruit", "watermelon lime", "blueberry lemon"]},
+  {menuCard: 'salt_factory.jpg', strength:["35mg", "50mg"], profile: ["fruit"], flavors: ["blue razz", "mystery", "mint", "melon lush"]},
+  {menuCard: 'met4_salts.jpg', strength:["24mg", "48mg"], profile: ["dessert"], flavors: ["golden ticket", "strawberry milk", "fairgrounds", "deep fried cream cake"]},
+  {menuCard: 'naked_salts.jpg', strength:["35mg", "50mg"], profile: ["fruit", "menthol", "tobacco"], flavors: ["hawaiian pog", "lava flow", "really berry", "melon kiwi", "strawberry pom", "melon", "berry", "polar breze", "brain freeze", "very cool", "american patriot", "mint"]},
+  {menuCard: 'reds_salts.jpg', strength:["30mg", "50mg"], profile: ["fruit", "menthol"], flavors: ["apple", "watermelon", "mango", "grape", "berries", "guava"]},
+  {menuCard: 'river_reserve_salts.jpg', strength:["25mg", "40mg"], profile: ["fruit", "cream", "dessert", "menthol"], flavors: ["blue island punch", "lemon drifter", "royal tobacco", "summer berry", "tangle", "vanilla waffle cone", "winter berry"]},
+  {menuCard: 'shijin_salts.jpg', strength:["50mg"], profile: ["fruit", "dessert"], flavors: ["dragon", "phoenix", "tortoise", "tiger"]},
+  {menuCard: 'svrf_salts.jpg', strength:["24mg", "48mg"], profile: ["fruit", "cream"], flavors: ["raspberry dragon fruit iced tea", "papaya mango cataloupe", "coconut and cream"]},
 ];
-nicSaltLibrary.sort();
-nicSaltLibrary.forEach(imageSrc => {
-  $('nicSaltContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/salts_images/${imageSrc} class = padding></div>`;
+saltLibrary.sort(function(a, b){
+    var letterA = a.menuCard.toLowerCase(), letterB = b.menuCard.toLowerCase()
+    if (letterA < letterB) //ascending
+        return -1 
+    if (letterA > letterB)
+        return 1
+    return 0
+})
+let fourOhFour = [{menuCard: '404.jpg'}]
+
+drawLibrary(saltLibrary);
+
+const searchBar = $("saltSearch");
+searchBar.addEventListener("keyup", e => {
+  const searchString = e.target.value.toLowerCase();
+  $('saltContainer').innerHTML = "";
+
+  const found = saltLibrary.filter(flavorCard => {
+    return (
+      flavorCard.menuCard.includes(searchString) ||
+      flavorCard.flavors.filter(flavor => flavor.includes(searchString)).length > 0 ||
+      flavorCard.strength.filter(strength => strength.includes(searchString)).length > 0 ||
+      flavorCard.profile.filter(profile => profile.includes(searchString)).length > 0
+    );
+  });
+  if(found.length > 0){
+      drawLibrary(found);
+  } else {
+    drawLibrary(fourOhFour);
+  }
 });
+
+function drawLibrary(q){
+  q.forEach(imageSrc => {
+      $('saltContainer').innerHTML += `<div class = "col"><div class = "row"><img src = resources/salt_images/${imageSrc.menuCard} class = padding></div>`;
+  });
+}
 
 function $(query){
 	return document.getElementById(query);
