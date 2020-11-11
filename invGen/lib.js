ecba59ecf071af1155bc889f7ec3753025c04a64
Johnthomas Lowing <42931512+SojournerDes@users.noreply.github.com>
2020-11-11 08:27:46 -0800

    Freebase HTML

--- invGen/lib.js
+++ invGen/lib.js
@@ -1,19 +1,17 @@
-let brandName = prompt("Enter name of brand: ");
+let brandName = prompt("Enter name of brand: ").toLowerCase().split(" ").join("");
+lib.innerHTML += `{brand: "${brandName}", shelfName: " ", product: "logo", blurb: " "}, <br>`
 let flavorCount = prompt("How many flavors are there?");
-let flavorInt = parseInt(flavorCount)+1;
-console.log(flavorInt);
-let lib = document.getElementById("lib");
+let flavorInt = parseInt(flavorCount);
 let product;
+let cleanProduct;
 let shelfName;
 let blurb;
-for(a = 0; a < flavorInt; a++){
+const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+const reg = new RegExp('[' + punctuation + ']', 'g');
+for(a = 0; a < flavorCount; a++){
 	shelfName = prompt("shelfName: (name as shown on website.)");
-	if(shelfName != "logo"){
-		product = shelfName.toLowerCase().split(" ").join("");
-	} else {
-		shelfName = " ";
-		product = "logo";
-	}
+	product = shelfName.toLowerCase().split(" ").join("");
+	cleanProduct = product.replace(reg, '');
 	blurb = prompt("flavor as shown on website");
-	lib.innerHTML += `{brand: '${brandName}', shelfName: '${shelfName}', product: '${product}', blurb: '${blurb}'}, <br>`
+	lib.innerHTML += `{brand: "${brandName}", shelfName: "${shelfName}", product: "${cleanProduct}", blurb: "${blurb}"}, <br>`
 };
