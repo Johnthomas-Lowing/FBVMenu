be3c29f4cdbb35c9767db599fe2c1bc3ce5bd354
Johnthomas Lowing <42931512+SojournerDes@users.noreply.github.com>
2020-11-11 21:39:07 -0800

    Testing gtag

---
+++ gtag.js
@@ -0,0 +1,5 @@
+async src="https://www.googletagmanager.com/gtag/js?id=UA-166255924-1">
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-166255924-1');
